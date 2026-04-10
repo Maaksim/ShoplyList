01f2e2c15893d510c2110960f59281ec3c541714
Maksym Vitovych <m.vitovych@lampalampa.net>
2026-04-10 16:57:26 +0300

    Added base firebase logic.

--- ShoplyList/ShoplyListApp.swift
+++ ShoplyList/ShoplyListApp.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct ShoplyListApp: App {
     @State var appCoordinator = AppCoordinator()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     private var tabBarCoordinator: TabBarCoordinator {
         appCoordinator.tabBarCoordinator
